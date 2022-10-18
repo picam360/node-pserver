cbbe72d04c20e8e47cc957d9391aae4b7385ae86
Takuma Hirayama <hirayama.takuma@ohmydigifab.com>
2022-10-19 01:02:19 +0900

    merge with pviewer

--- setup.js
+++ setup.js
@@ -14,7 +14,14 @@ try{
 }
 
 try{
-	execSync('git clone --depth 1 https://github.com/picam360/pviewer.git www -b v0.10', {cwd : __dirname});
+	execSync('git clone --depth 1 https://github.com/picam360/pviewer.git www -b v0.13', {cwd : __dirname});
 }catch(err){
 	console.log("error on git:" + err);
+}
+
+try{
+	fs.copyFileSync("www/plugins/network/signaling.js", "plugins/network/signaling.js");
+	fs.copyFileSync("www/plugins/network/meeting.js", "plugins/network/meeting.js");
+}catch(err){
+	console.log("copy files:" + err);
 }
