5d8d3c4cef6f421f7c9bb805c7c1b637f36c75d6
hirayama.takuma <hirayama.takuma@ohmydigifab.com>
2022-11-17 22:25:30 +0900

    setup_init.sh

--- docker/jetson/setup_init.sh
+++ docker/jetson/setup_init.sh
@@ -3,6 +3,7 @@
 PSERVER_VERSION=$1
 CONFIG_PATH=$2
 
-sudo apt-get install nano
+sudo apt-get update
+sudo apt-get -y install nano
 bash setup_image.sh $PSERVER_VERSION
 bash setup_service.sh $CONFIG_PATH
