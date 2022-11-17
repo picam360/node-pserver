baa99889c5bc6fa2423e7d6c035882f02f6ca391
hirayama.takuma <hirayama.takuma@ohmydigifab.com>
2022-11-17 22:02:01 +0900

    config_dir_param

--- docker/jetson/setup_container.sh
+++ docker/jetson/setup_container.sh
@@ -2,6 +2,14 @@
 
 PSERVER_VERSION=$1
 CONFIG_PATH=$2
+
+if [[ $CONFIG_PATH =~ ^/ ]]; then
+    CONFIG_DIR=$(dirname $CONFIG_PATH)
+else
+    CONFIG_DIR=$(pwd)/$(dirname $CONFIG_PATH)
+fi
+CONFIG_DIR_PARAM="-v $CONFIG_DIR:$CONFIG_DIR"
+
 if sudo docker image inspect pserver:${PSERVER_VERSION} 1> /dev/null 2>/dev/null; then
     echo "pserver:${PSERVER_VERSION} found"
     sudo docker container rm pserver
@@ -9,7 +17,7 @@ else
     echo "pserver:${PSERVER_VERSION} not found"
     sudo docker build -t pserver:${PSERVER_VERSION} --build-arg PSERVER_VERSION=${PSERVER_VERSION} -f Dockerfile.jetson .
 fi
-sudo docker run -d --name pserver --runtime nvidia -p 9001:9001 -v /etc/pserver:/etc/pserver --net=host -e DISPLAY=:0 -e PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native -v ${XDG_RUNTIME_DIR}/pulse/native:${XDG_RUNTIME_DIR}/pulse/native --privileged pserver:${PSERVER_VERSION} -c ${CONFIG_PATH}
+sudo docker run -d --name pserver --runtime nvidia -p 9001:9001 $CONFIG_DIR_PARAM --net=host -e DISPLAY=:0 -e PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native -v ${XDG_RUNTIME_DIR}/pulse/native:${XDG_RUNTIME_DIR}/pulse/native --privileged pserver:${PSERVER_VERSION} -c ${CONFIG_PATH}
 sed -e "s/@PSERVER_VERSION@/${PSERVER_VERSION}/g" pserver.container.in | sudo tee /usr/bin/pserver
 sudo chmod +x /usr/bin/pserver
 
