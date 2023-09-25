2a397bed75621e064d5f6018b9ff141e0de63dc2
Takuma Hirayama <hirayama.takuma@ohmydigifab.com>
2023-09-25 14:35:24 +0900

    l4t version

--- docker/jetson/setup_container.sh
+++ docker/jetson/setup_container.sh
@@ -15,7 +15,10 @@ if sudo docker image inspect pserver:${PSERVER_VERSION} 1> /dev/null 2>/dev/null
     sudo docker container rm pserver
 else
     echo "pserver:${PSERVER_VERSION} not found"
-    sudo docker build -t pserver:${PSERVER_VERSION} --build-arg PSERVER_VERSION=${PSERVER_VERSION} -f Dockerfile.jetson .
+    L4T_VERSION_INFO=$(dpkg-query --showformat='${Version}' --show nvidia-l4t-core)
+    L4T_VERSION=${version_info%%-*}
+    echo "use L4T_VERSION=${L4T_VERSION}"
+    sudo docker build -t pserver:${PSERVER_VERSION} --build-arg PSERVER_VERSION=${PSERVER_VERSION} --build-arg L4T_VERSION=${L4T_VERSION} -f Dockerfile.jetson .
 fi
 sudo docker run -d --name pserver --runtime nvidia -p 9001:9001 $CONFIG_DIR_PARAM --net=host -e DISPLAY=:0 -e PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native -v ${XDG_RUNTIME_DIR}/pulse/native:${XDG_RUNTIME_DIR}/pulse/native -v /var/run/dbus:/var/run/dbus --privileged pserver:${PSERVER_VERSION} -c ${CONFIG_PATH}
 sed -e "s/@PSERVER_VERSION@/${PSERVER_VERSION}/g" pserver.container.in | sudo tee /usr/bin/pserver
--- docker/jetson/setup_image.sh
+++ docker/jetson/setup_image.sh
@@ -5,7 +5,10 @@ if sudo docker image inspect pserver:${PSERVER_VERSION} 1> /dev/null 2>/dev/null
     echo "pserver:${PSERVER_VERSION} found"
 else
     echo "pserver:${PSERVER_VERSION} not found"
-    sudo docker build -t pserver:${PSERVER_VERSION} --build-arg PSERVER_VERSION=${PSERVER_VERSION} -f Dockerfile.jetson .
+    L4T_VERSION_INFO=$(dpkg-query --showformat='${Version}' --show nvidia-l4t-core)
+    L4T_VERSION=${version_info%%-*}
+    echo "use L4T_VERSION=${L4T_VERSION}"
+    sudo docker build -t pserver:${PSERVER_VERSION} --build-arg PSERVER_VERSION=${PSERVER_VERSION} --build-arg L4T_VERSION=${L4T_VERSION} -f Dockerfile.jetson .
 fi
 sed -e "s/@PSERVER_VERSION@/${PSERVER_VERSION}/g" pserver.image.in | sudo tee /usr/bin/pserver
 sudo chmod +x /usr/bin/pserver
