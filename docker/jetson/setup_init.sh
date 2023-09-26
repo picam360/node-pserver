1eb661c8f8c9abde5500cf7af0cc6321ee4f3c2a
Takuma Hirayama <hirayama.takuma@ohmydigifab.com>
2023-09-26 12:30:58 +0900

    nvfan

--- docker/jetson/setup_init.sh
+++ docker/jetson/setup_init.sh
@@ -7,3 +7,5 @@ sudo apt-get update
 sudo apt-get -y install nano nvidia-container-runtime nvidia-docker2
 bash setup_image.sh $PSERVER_VERSION
 bash setup_service.sh $CONFIG_PATH
+
+sudo sed -i 's/FAN_DEFAULT_PROFILE .*/FAN_DEFAULT_PROFILE cool/g' /etc/nvfancontrol.conf
