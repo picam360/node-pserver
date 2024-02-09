902601124b2569e70563f08581d363ebe15d8698
Takuma Hirayama <hirayama.takuma@ohmydigifab.com>
2024-02-09 13:01:52 +0900

    curl

--- docker/jetson/node/setup_nodebrew.sh
+++ docker/jetson/node/setup_nodebrew.sh
@@ -5,13 +5,12 @@
 
 if hash nodebrew 2>/dev/null; then
     echo "nodebrew is already installed."
-    exit 0
+else
+    sudo apt-get -y install curl
+
+    curl -L git.io/nodebrew | perl - setup
+    echo "export PATH=$HOME/.nodebrew/current/bin:\$PATH" >> $HOME/.bashrc
+    export PATH=$HOME/.nodebrew/current/bin:$PATH
+    sed -i -z "s/} elsif (\$machine =~ m\/aarch64\/) {\n        \$arch = 'armv7l';/} elsif (\$machine =~ m\/aarch64\/) {\n        \$arch = 'arm64'/g" $HOME/.nodebrew/nodebrew
+    nodebrew install v14.17.1 && nodebrew use v14.17.1
 fi
-
-sudo apt-get -y install curl
-
-curl -L git.io/nodebrew | perl - setup
-echo "export PATH=$HOME/.nodebrew/current/bin:\$PATH" >> $HOME/.bashrc
-export PATH=$HOME/.nodebrew/current/bin:$PATH
-sed -i -z "s/} elsif (\$machine =~ m\/aarch64\/) {\n        \$arch = 'armv7l';/} elsif (\$machine =~ m\/aarch64\/) {\n        \$arch = 'arm64'/g" $HOME/.nodebrew/nodebrew
-nodebrew install v14.17.1 && nodebrew use v14.17.1
