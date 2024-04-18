5d9ee53c663f4bc716556f35e0057e48a095b118
Takuma Hirayama <hirayama.takuma@ohmydigifab.com>
2024-04-18 16:29:05 +0900

    setup_init.sh

--- docker/jetson/setup_init.sh
+++ docker/jetson/setup_init.sh
@@ -4,7 +4,9 @@ PSERVER_VERSION=$1
 CONFIG_PATH=$2
 
 sudo apt-get update
-sudo apt-get -y install nano nvidia-container-runtime nvidia-docker2
+sudo apt-get -y install nano p7zip-full p7zip-rar nvidia-container-runtime nvidia-docker2
+sudo systemctl restart docker
+
 bash setup_image.sh $PSERVER_VERSION
 bash setup_service.sh $CONFIG_PATH
 
