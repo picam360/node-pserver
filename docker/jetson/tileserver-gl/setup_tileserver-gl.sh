0e3291fc63135aa02a04aad9969cf0e17395ad30
Takuma Hirayama <hirayama.takuma@ohmydigifab.com>
2024-08-29 10:23:39 +0900

    tileserver-gl

---
+++ docker/jetson/tileserver-gl/setup_tileserver-gl.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker pull
+
+DATA_PATH=/var/mbtiles
+sed -e "s%@DATA_PATH@%${DATA_PATH}%" tileserver-gl.service.in | sudo tee /etc/systemd/system/tileserver-gl.service
+sudo systemctl daemon-reload
+sudo systemctl restart tileserver-gl.service
+sudo systemctl enable tileserver-gl.service
