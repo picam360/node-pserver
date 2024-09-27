1312fab91c37d221778d8675b151f1e48116345f
hirayama.takuma <hirayama.takuma@ohmydigifab.com>
2024-09-27 12:17:48 +0900

    support node-pstcore version dynamic change

---
+++ update-dependencies.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
+// Get the path to package.json
+const packageJsonPath = path.resolve(__dirname, 'package.json');
+
+// Read package.json
+const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+
+// Get the version from the environment variable (if not set, do nothing)
+const newVersion = process.env.NODE_PSTCORE_VERSION;
+
+if (newVersion) {
+  // Change the version of test_module to the one specified by the environment variable
+  packageJson.dependencies['node-pstcore'] = newVersion;
+
+  // Update package.json
+  fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2), 'utf8');
+  console.log(`Updated node-pstcore version to ${newVersion} in package.json`);
+}
